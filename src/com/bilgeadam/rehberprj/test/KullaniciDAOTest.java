87ce454a5796cbb6ec3b7ea3c9d7a05430277f92
Bora Yüret <borayuret@gmail.com>
2021-12-11 13:49:30 +0300

    KullaniciDAOTest eklendi.

---
+++ src/com/bilgeadam/rehberprj/test/KullaniciDAOTest.java
@@ -0,0 +1,30 @@
+package com.bilgeadam.rehberprj.test;
+
+import com.bilgeadam.rehberprj.dao.KullaniciDAO;
+import com.bilgeadam.rehberprj.dto.KullaniciDTO;
+
+import java.sql.SQLException;
+
+public class KullaniciDAOTest {
+    public static void main(String[] args) {
+
+        try {
+            KullaniciDTO kullanici = new KullaniciDTO();
+            kullanici.setKullaniciAdi("admin");
+            kullanici.setSifre("admin1235");
+
+            boolean sonuc = KullaniciDAO.giriseYetkilimi(kullanici);
+
+            if(sonuc)
+                System.out.println("Girişe yetkilidir.");
+            else
+                System.out.println("Girişe yetkili değildir!");
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
