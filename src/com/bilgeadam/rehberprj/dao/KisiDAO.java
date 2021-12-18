a8ab32044ed3aac7b3f4f3d5262fb4dff5e0234f
Bora Yüret <borayuret@gmail.com>
2021-12-18 13:36:11 +0300

    KisiDAO classına ekle metodu eklendi.

--- src/com/bilgeadam/rehberprj/dao/KisiDAO.java
+++ src/com/bilgeadam/rehberprj/dao/KisiDAO.java
@@ -3,6 +3,7 @@ package com.bilgeadam.rehberprj.dao;
 import com.bilgeadam.rehberprj.dto.KisiDTO;
 import com.bilgeadam.rehberprj.dto.KullaniciDTO;
 import com.bilgeadam.rehberprj.sec.AES;
+import com.bilgeadam.rehberprj.util.CevirmeIslemleri;
 import com.bilgeadam.rehberprj.util.Sabitler;
 import com.bilgeadam.rehberprj.vt.VTBaglanti;
 
@@ -13,45 +14,33 @@ import java.sql.SQLException;
 
 public class KisiDAO {
 
- //   public static boolean ekle(KisiDTO kisi)
-   //         throws SQLException, ClassNotFoundException {
+    public static boolean ekle(KisiDTO kisi)
+          throws SQLException, ClassNotFoundException {
 
-//        Connection conn = VTBaglanti.baglantiGetir();
-//
-//        String sorgu = "insert into kisi(no, ad, soyad, maas, dogtar, mobil_tel) values(?,?,?,?,?,?)";
-//
-//        PreparedStatement ps = conn.prepareStatement(sorgu);
-//
-//        ps.setString(1, kullanici.getKullaniciAdi());
-//        ps.setInt(2, kullanici.getAktif());
-//
-//        ResultSet rs = ps.executeQuery();
-//
-//        while(rs.next())
-//        {
-//            vtSifre = rs.getString("sifre");
-//        }
-//        rs.close();
-//
-//        ps.close();
-//        VTBaglanti.baglantiKapat(conn);
-//
-//        String cozulmus_sifre = null;
-//
-//        if(vtSifre != null) {
-//            cozulmus_sifre = AES.decrypt(vtSifre, Sabitler.SECRET_KEY);
-//        }
-//        else{
-//            // vtSifre null ise aşağıdaki eşitliği kontrol etmeye gerek yok.
-//            // Buradan metod sonucunu false olarak döndürüyorum.
-//            return false;
-//        }
-//
-//        if(kullanici.getSifre().equals(cozulmus_sifre))
-//            return true;
-//        else
-//            return false;
-//
-//    }
+        Connection conn = VTBaglanti.baglantiGetir();
+
+        String sorgu = "insert into kisi(no, ad, soyad, maas, dogtar, mobil_tel) values(?,?,?,?,?,?)";
+
+        PreparedStatement ps = conn.prepareStatement(sorgu);
+
+        ps.setInt(1, kisi.getNo());
+        ps.setString(2, kisi.getAd());
+        ps.setString(3, kisi.getSoyad());
+        ps.setDouble(4, kisi.getMaas());
+        ps.setDate(5, CevirmeIslemleri.utilToSqlDate(kisi.getDogtar()));
+       // ps.setDate(5, new java.sql.Date(kisi.getDogtar().getTime()));
+        ps.setString(6, kisi.getMobilTel());
+
+        int sonuc = ps.executeUpdate();
+
+        ps.close();
+        VTBaglanti.baglantiKapat(conn);
+
+        if (sonuc > 0)
+            return true;
+        else
+            return false;
+
+    }
 
 }
